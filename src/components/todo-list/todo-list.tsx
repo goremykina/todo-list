1f62a62b790a4c107c8d19756b83cbd7edc72d24
Alina Goremykina <a.goremykina98@gmail.com>
2024-09-23 17:29:41 +0200

    Add and use react-beautiful-dnd

--- src/components/todo-list/todo-list.tsx
+++ src/components/todo-list/todo-list.tsx
@@ -3,31 +3,25 @@ import { FormEvent, useState } from "react";
 import Button from "../button/button.tsx";
 import { TodoItem } from "../../models/todo-item.ts";
 import { useCreateToDoItem, useGetTodoItems } from "../../api/todo-items.api.ts";
+import { Droppable } from "react-beautiful-dnd";
+import OneTodoItem from "../todo-item/todo-item.tsx";
 
 type PropsType = {
     title: string,
     items:  TodoItem[],
     onTodoItemAdded: (todoItem: TodoItem) => void,
+    onTodoItemDeleted: (todoItem: TodoItem) => void,
+    onTodoItemUpdated: (todoItem: TodoItem) => void,
+    droppableId: string
 };
 
 export default function TodoList(props: PropsType) {
-    const { title , items, onTodoItemAdded} = props;
-    console.log(items)
+    const { title , items, onTodoItemAdded, onTodoItemDeleted, onTodoItemUpdated, droppableId} = props;
     const [newItemTitle, setNewItemTitle] = useState('');
     const [isCreateMode, setIsCreateMode] = useState<boolean>(false);
-    const {
-        loading,
-        // getTodoItems,
-    } = useGetTodoItems();
+    const { loading } = useGetTodoItems();
     const { createTodoItem } = useCreateToDoItem();
 
-    // useEffect(() => {
-    //     getTodoItems()
-    //        .then(items => {
-    //            console.log(items);
-    //            setTodoItems(items)
-    //        });
-    // }, []);
 
     const submitNewItem = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -43,13 +37,6 @@ export default function TodoList(props: PropsType) {
         setIsCreateMode(true)
     }
 
-    const onDelete = (id: string) => {
-        // const before = todoItems.slice(0, indexEl);
-        // const after = todoItems.slice(indexEl + 1);
-        //
-        // setTodoItems(before.concat(after))
-        console.log(id)
-    }
 
     if (loading) {
         return <span>Loading...</span>;
@@ -58,27 +45,33 @@ export default function TodoList(props: PropsType) {
     return (
         <div className="todo-list" >
             <h2 className='todo-section-title'>{title}</h2>
-            <div className="todo-list-items">
-
-                {items.map(((item: TodoItem) => (
-                    <div className='todo-item'>
-                        <input type='checkbox'/>
-                        <textarea key={item.id} className='todo-item-header'>{item.title}</textarea>
-                        <Button
-                            text='Delete'
-                            onClick={() => onDelete(item.id)}
-                        ></Button>
+            <Droppable droppableId={droppableId}>
+                {(provided) => (
+                    <div
+                        className="todo-list-container"
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                    >
+                        {items.map(((item: TodoItem, index) => (
+                            <OneTodoItem
+                                key={item.id}
+                                item={item}
+                                onTodoItemDeleted={onTodoItemDeleted}
+                                onTodoItemUpdated={onTodoItemUpdated}
+                                index={index} />
+                        )))}
+                        {provided.placeholder}
                     </div>
-                )))}
-            </div>
+                )}
+            </Droppable>
 
             {isCreateMode && (
                 <form onSubmit={submitNewItem} className='todo-create-form'>
                     <textarea
                         required
                         className="todo-item-header"
-                        onChange={e => setNewItemTitle(e.target.value)}
-                        value={newItemTitle}/>
+                        value={newItemTitle}
+                        onChange={e => setNewItemTitle(e.target.value)}/>
                     <Button
                         text='Create'
                     />
@@ -88,7 +81,8 @@ export default function TodoList(props: PropsType) {
             {!isCreateMode && (
                 <Button
                     text='Add Task'
-                    onClick={() => goToCreateMode()}></Button>
+                    onClick={() => goToCreateMode()}>
+                </Button>
             )}
         </div>
     )
